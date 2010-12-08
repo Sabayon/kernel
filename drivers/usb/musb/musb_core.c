69a7a7f33433979b35ddff8e249ed9c5b2a9647b
Ravi B <ravibabu@ti.com>
2010-12-08 17:28:03 +0530

    musb: Adding musb->id to support multi-musb controller
    Added musb->id field to differenciate two controllers.
    
    Signed-off-by: Ravi B <ravibabu@ti.com>
    Signed-off-by: Ajay Kumar Gupta <ajay.gupta@ti.com>
    Signed-off-by: Vaibhav Hiremath <hvaibhav@ti.com>

--- drivers/usb/musb/musb_core.c
+++ drivers/usb/musb/musb_core.c
@@ -1880,6 +1880,7 @@ musb_init_controller(struct device *dev, int nIrq, void __iomem *ctrl,
 	int			status;
 	struct musb		*musb;
 	struct musb_hdrc_platform_data *plat = dev->platform_data;
+	struct platform_device *pdev = to_platform_device(dev);
 
 	/* The driver might handle more features than the board; OK.
 	 * Fail when the board needs a feature that's not enabled.
@@ -1908,6 +1909,8 @@ musb_init_controller(struct device *dev, int nIrq, void __iomem *ctrl,
 	musb->board_set_power = plat->set_power;
 	musb->min_power = plat->min_power;
 	musb->ops = plat->platform_ops;
+	musb->id = pdev->id;
+
 	if (fifo_mode == -1)
 		fifo_mode = musb->ops->fifo_mode;
 

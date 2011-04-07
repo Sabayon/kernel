ffcb3ec5230efd54bf0a6a87a5d51e4faee41b5a
Andy Green <andy.green@linaro.org>
2011-04-07 08:42:12 +0100

    OMAP4: PANDA: remove reference to hsmmc reset
    Signed-off-by: Andy Green <andy.green@linaro.org>

--- arch/arm/mach-omap2/devices.c
+++ arch/arm/mach-omap2/devices.c
@@ -700,7 +700,6 @@ static int __init omap2_init_devices(void)
 	 * please keep these calls, and their implementations above,
 	 * in alphabetical order so they're easier to sort through.
 	 */
-	omap_hsmmc_reset();
 	omap_init_abe();
 	omap_init_audio();
 	omap_init_camera();

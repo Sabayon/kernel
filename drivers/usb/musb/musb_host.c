b5b499f8c85b715e580aa6ec0fd83edc20e0936f
Ravi B <ravibabu@ti.com>
2011-06-14 15:04:19 +0530

    usb: musb: pass musb pointer to is_inventra_dma helper
    This patch passes musb pointer to is_inventra_dma to
    prepare for support runtime is_inventra_dma.
    
    Signed-off-by: Ming Lei <tom.leiming@gmail.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Ajay Kumar Gupta <ajay.gupta@ti.com>
    Signed-off-by: Vaibhav Hiremath <hvaibhav@ti.com>

--- drivers/usb/musb/musb_host.c
+++ drivers/usb/musb/musb_host.c
@@ -615,7 +615,7 @@ static bool musb_tx_dma_program(struct dma_controller *dma,
 	u16			csr;
 	u8			mode;
 
-	if (is_inventra_dma()) {
+	if (is_inventra_dma(hw_ep->musb)) {
 		if (length > channel->max_len)
 			length = channel->max_len;
 
@@ -1525,7 +1525,7 @@ void musb_host_rx(struct musb *musb, u8 epnum)
 
 	/* FIXME this is _way_ too much in-line logic for Mentor DMA */
 
-	if (!is_inventra_dma() && (rx_csr & MUSB_RXCSR_H_REQPKT))  {
+	if (!is_inventra_dma(musb) && (rx_csr & MUSB_RXCSR_H_REQPKT))  {
 		/* REVISIT this happened for a while on some short reads...
 		 * the cleanup still needs investigation... looks bad...
 		 * and also duplicates dma cleanup code above ... plus,
@@ -1555,7 +1555,7 @@ void musb_host_rx(struct musb *musb, u8 epnum)
 			| MUSB_RXCSR_RXPKTRDY);
 		musb_writew(hw_ep->regs, MUSB_RXCSR, val);
 
-		if (is_inventra_dma()) {
+		if (is_inventra_dma(musb)) {
 			if (usb_pipeisoc(pipe)) {
 				struct usb_iso_packet_descriptor *d;
 
@@ -1613,7 +1613,7 @@ void musb_host_rx(struct musb *musb, u8 epnum)
 		}
 
 		/* we are expecting IN packets */
-		if (is_inventra_dma() && dma) {
+		if (is_inventra_dma(musb) && dma) {
 			struct dma_controller	*c;
 			u16			rx_count;
 			int			ret, length;

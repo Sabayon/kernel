00f9117d6ca8d56933efd2945686c305a428b308
Jassi Brar <jaswinder.singh@linaro.org>
2011-07-24 20:59:53 +0100

    Subject: [PATCH 103/104] OMAP4: Panda: Add twl6040 codec platform support
    Enable the twl6040 codec on PandaBoard.
    
    Signed-off-by: Jassi Brar <jaswinder.singh@linaro.org>

--- arch/arm/mach-omap2/board-omap4panda.c
+++ arch/arm/mach-omap2/board-omap4panda.c
@@ -289,8 +289,19 @@ static int __init omap4_twl6030_hsmmc_init(struct omap2_hsmmc_info *controllers)
 	return 0;
 }
 
+static struct twl4030_codec_data twl6040_codec;
+
+static struct twl4030_audio_data twl6040_audio = {
+	.codec		= &twl6040_codec,
+	.audpwron_gpio	= 127,
+	.naudint_irq	= OMAP44XX_IRQ_SYS_2N,
+	.irq_base	= TWL6040_CODEC_IRQ_BASE,
+};
+
 /* Use the common PMIC configuration for Panda */
-static struct twl4030_platform_data omap4_panda_twldata;
+static struct twl4030_platform_data omap4_panda_twldata = {
+	.audio		= &twl6040_audio,
+};
 
 /*
  * Display monitor features are burnt in their EEPROM as EDID data. The EEPROM

0ee5266fad22107531ba987600bdabb44a4de5e5
Liam Girdwood <lrg@ti.com>
2011-07-24 20:59:38 +0100

    Subject: [PATCH 055/104] ASoC: dapm - add methods to get snd and soc card from dapm
    In preparation for dynamic PCM support (AKA ASoC DSP)
    
    Add methods to get the soc_card and snd_card from a dapm context.
    
    Signed-off-by: Liam Girdwood <lrg@ti.com>

--- sound/soc/soc-dapm.c
+++ sound/soc/soc-dapm.c
@@ -124,6 +124,31 @@ static inline struct snd_soc_dapm_widget *dapm_cnew_widget(
 	return kmemdup(_widget, sizeof(*_widget), GFP_KERNEL);
 }
 
+static inline struct snd_card *dapm_get_snd_card(
+	struct snd_soc_dapm_context *dapm)
+{
+	if (dapm->codec)
+		return dapm->codec->card->snd_card;
+	else if (dapm->platform)
+		return dapm->platform->card->snd_card;
+
+	dev_err(dapm->dev, "no snd_card for dapm context\n");
+	return NULL;
+}
+
+static inline struct snd_soc_card *dapm_get_soc_card(
+		struct snd_soc_dapm_context *dapm)
+{
+	if (dapm->codec)
+		return dapm->codec->card;
+	else if (dapm->platform)
+		return dapm->platform->card;
+
+	dev_err(dapm->dev, "no soc_card for dapm context\n");
+	return NULL;
+}
+
+
 static int soc_widget_read(struct snd_soc_dapm_widget *w, int reg)
 {
 	if (w->codec)

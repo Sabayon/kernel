c34ab0e964e954326d8640d1f7ad0e6aa17b472c
Afzal Mohammed <afzal@ti.com>
2011-09-29 14:59:09 +0530

    arm:omap:am33xx: Voltage domain data
    Currently this is being added to to prevent non
    registration of power & clock domains, nothing
    w.r.t actual hardware is done.
    
    TODO: Revisit the change later
    
    Signed-off-by: Afzal Mohammed <afzal@ti.com>

---
+++ arch/arm/mach-omap2/voltagedomains33xx_data.c
@@ -0,0 +1,32 @@
+/*
+ * AM33XX voltage domain data
+ *
+ * Copyright (C) 2011 Texas Instruments, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+#include "voltage.h"
+
+static struct voltagedomain am33xx_voltdm_core = {
+	.name = "core",
+};
+
+static struct voltagedomain am33xx_voltdm_rtc = {
+	.name = "rtc",
+};
+
+static struct voltagedomain *voltagedomains_am33xx[] __initdata = {
+	&am33xx_voltdm_core,
+	&am33xx_voltdm_rtc,
+	NULL,
+};
+
+void __init am33xx_voltagedomains_init(void)
+{
+	voltdm_init(voltagedomains_am33xx);
+}

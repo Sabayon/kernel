c3f05349ee4696f2c210cdd359aa7b02f9b1344a
Vaibhav Bedia <vaibhav.bedia@ti.com>
2011-10-11 01:40:12 +0530

    ARM:omap3:am33xx: HACK for bypassing RTC hwmod
    Temp patch to avoid rtc clock getting reset
    Needs to be investigated
    
    Signed-off-by: Vaibhav Bedia <vaibhav.bedia@ti.com>

--- arch/arm/mach-omap2/omap_hwmod_33xx_data.c
+++ arch/arm/mach-omap2/omap_hwmod_33xx_data.c
@@ -2430,7 +2430,9 @@ static __initdata struct omap_hwmod *am33xx_hwmods[] = {
 	/* ocpwp class */
 	&am33xx_ocpwp_hwmod,
 	/* rtc class */
+#if 0
 	&am33xx_rtc_hwmod,
+#endif
 	/* sha0 class */
 	&am33xx_sha0_hwmod,
 	/* smartreflex class */

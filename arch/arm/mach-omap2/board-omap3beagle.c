110acf2041c326f81373011f3c55964e37dee9b4
Robert Nelson <robertcnelson@gmail.com>
2011-10-10 11:12:40 -0500

    expansion: add buddy param for expansionboard names
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- arch/arm/mach-omap2/board-omap3beagle.c
+++ arch/arm/mach-omap2/board-omap3beagle.c
@@ -21,6 +21,7 @@
 #include <linux/io.h>
 #include <linux/leds.h>
 #include <linux/gpio.h>
+#include <linux/irq.h>
 #include <linux/input.h>
 #include <linux/gpio_keys.h>
 #include <linux/opp.h>
@@ -155,6 +156,8 @@ static void __init omap3_beagle_init_rev(void)
 		omap3_beagle_version = OMAP3BEAGLE_BOARD_UNKN;
 	}
 }
+
+char expansionboard_name[16];
 
 static struct mtd_partition omap3beagle_nand_partitions[] = {
 	/* All the partition sizes are listed in terms of NAND block size */
@@ -474,6 +477,15 @@ static struct omap_board_mux board_mux[] __initdata = {
 };
 #endif
 
+static int __init expansionboard_setup(char *str)
+{
+	if (!str)
+		return -EINVAL;
+	strncpy(expansionboard_name, str, 16);
+	printk(KERN_INFO "Beagle expansionboard: %s\n", expansionboard_name);
+	return 0;
+}
+
 static void __init beagle_opp_init(void)
 {
 	int r = 0;
@@ -551,6 +563,8 @@ static void __init omap3_beagle_init(void)
 	beagle_display_init();
 	beagle_opp_init();
 }
+
+early_param("buddy", expansionboard_setup);
 
 MACHINE_START(OMAP3_BEAGLE, "OMAP3 Beagle Board")
 	/* Maintainer: Syed Mohammed Khasim - http://beagleboard.org */

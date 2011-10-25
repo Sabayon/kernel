d213106185cfc0f0ca862ebbc1b9d83485e89223
Hari Kanigeri <h-kanigeri2@ti.com>
2011-10-25 10:51:05 +0800

    omap:iommu-event notification shouldn't be blocking call
    iommu_notify_event function is called from interrupt context, and this
    function is using internally blocking_notifier_call_chain call that could
    sleep.
    The fix is changing  blocking_notifier to raw_notifier and let the callers
    of the iommu_notify_event handle the protection.
    
    iommu_notify_event is used for 2 purposes. One is for MMU FAULT notification
    to the device handler where the devh sets the error state to Fault and prevents
    the broadcasting of any iommu close notifications. This shouldn't be blocking.
    
    Second, it is used to broadcast the notifications of the Process ID that closed
    the iommu handle to remote processor. This could be blocking.
    Reported by Hari Nagalla
    
    Signed-off-by: Hari Kanigeri <h-kanigeri2@ti.com>
    Signed-off-by: Hari Nagalla <hnagalla@ti.com>

--- arch/arm/plat-omap/include/plat/iommu.h
+++ arch/arm/plat-omap/include/plat/iommu.h
@@ -48,7 +48,7 @@ struct iommu {
 	struct list_head	mmap;
 	struct mutex		mmap_lock; /* protect mmap */
 
-	struct blocking_notifier_head	notifier;
+	struct raw_notifier_head	notifier;
 
 	u32 da_start;
 	u32 da_end;

d85c62e1925180eb657fb5cf5b1f245289f4073b
Hari Kanigeri <h-kanigeri2@ti.com>
2011-10-25 10:51:23 +0800

    OMAP: SYSLINK: cacheflush
    Revert "ARM: dma-mapping: remove dmac_clean_range and dmac_inv_range"
    
        This reverts commit 702b94bff3c50542a6e4ab9a4f4cef093262fe65
    
    Signed-off-by: Hari Kanigeri <h-kanigeri2@ti.com>

--- arch/arm/include/asm/glue-cache.h
+++ arch/arm/include/asm/glue-cache.h
@@ -140,6 +140,8 @@
 
 #define dmac_map_area			__glue(_CACHE,_dma_map_area)
 #define dmac_unmap_area			__glue(_CACHE,_dma_unmap_area)
+#define dmac_inv_range			__glue(_CACHE,_dma_inv_range)
+#define dmac_clean_range		__glue(_CACHE,_dma_clean_range)
 #define dmac_flush_range		__glue(_CACHE,_dma_flush_range)
 #endif
 

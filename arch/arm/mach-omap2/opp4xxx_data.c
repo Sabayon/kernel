ebbaff10e9dfbcb69a5c4e9fb6ca97ae5ff62ad2
Vishwanath BS <vishwanath.bs@ti.com>
2011-10-25 10:42:22 +0800

    OMAP4460 OPP: Enable Nitro OPP dynamically
    Based on supported OPP, enable Nitro OPP.
    
    Signed-off-by: Vishwanath BS <vishwanath.bs@ti.com>

--- arch/arm/mach-omap2/opp4xxx_data.c
+++ arch/arm/mach-omap2/opp4xxx_data.c
@@ -19,8 +19,10 @@
  * GNU General Public License for more details.
  */
 #include <linux/module.h>
+#include <linux/opp.h>
 
 #include <plat/cpu.h>
+#include <plat/omap_device.h>
 
 #include "control.h"
 #include "omap_opp_data.h"
@@ -204,6 +206,32 @@ static struct omap_opp_def __initdata omap446x_opp_def_list[] = {
 	/* TODO: add DSP, aess, fdif, gpu */
 };
 
+	/**
+	 * omap4_opp_enable() - Enable the OPP corresponding to freq
+	 *
+	 */
+	static int __init omap4_opp_enable(unsigned long freq)
+	{
+	       int r = -ENODEV;
+	       struct omap_hwmod *oh_mpu = omap_hwmod_lookup("mpu");
+	       struct platform_device *pdev;
+
+	       if (!oh_mpu || !oh_mpu->od) {
+	               goto err;
+	       } else {
+	               pdev = &oh_mpu->od->pdev;
+
+	               r = opp_enable(&pdev->dev, freq);
+	               if (r < 0) {
+	                       dev_err(&pdev->dev,
+	                               "opp_enable() failed for mpu@%ld", freq);
+	                       goto err;
+	               }
+	       }
+	err:
+		return r;
+	}
+
 /**
  * omap4_opp_init() - initialize omap4 opp table
  */
@@ -213,12 +241,17 @@ int __init omap4_opp_init(void)
 
 	if (!cpu_is_omap44xx())
 		return r;
-	if (cpu_is_omap443x())
+	if (cpu_is_omap443x()){
 		r = omap_init_opp_table(omap443x_opp_def_list,
 			ARRAY_SIZE(omap443x_opp_def_list));
-	else if (cpu_is_omap446x())
+	} else if (cpu_is_omap446x()) {
 		r = omap_init_opp_table(omap446x_opp_def_list,
 			ARRAY_SIZE(omap446x_opp_def_list));
+		if (omap4_has_mpu_1_5ghz())
+			omap4_opp_enable(1500000000);
+		if (omap4_has_mpu_1_2ghz())
+			omap4_opp_enable(1200000000);
+	}
 	return r;
 }
 device_initcall(omap4_opp_init);

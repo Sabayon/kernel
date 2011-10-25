bbbf7cf3de1b6a3dc72e3c4ed0e86a455a48accb
Andy Green <andy.green@linaro.org>
2011-10-25 10:43:01 +0800

    omap2 panda kim suspend resume stubs
    Signed-off-by: Andy Green <andy.green@linaro.org>

--- arch/arm/mach-omap2/board-omap4panda.c
+++ arch/arm/mach-omap2/board-omap4panda.c
@@ -57,12 +57,26 @@
 #define HDMI_GPIO_HPD 60 /* Hot plug pin for HDMI */
 #define HDMI_GPIO_LS_OE 41 /* Level shifter for HDMI */
 
+static int
+panda_kim_suspend(struct platform_device *pdev, pm_message_t msg)
+{
+	return 0;
+}
+
+static int
+panda_kim_resume(struct platform_device *pdev)
+{
+	return 0;
+}
 
 struct ti_st_plat_data panda_bt_platform_data = {
         .nshutdown_gpio = 46,
         .dev_name = "/dev/ttyO1",
         .flow_cntrl = 1,
         .baud_rate = 3000000,
+        .suspend = panda_kim_suspend,
+        .resume = panda_kim_resume,
+
 };
 
 static struct platform_device wl1271_device = {

838afc2284efb2ada59099d1be4c5667af43e6bc
Dave Martin <dave.martin@linaro.org>
2011-10-25 10:47:46 +0800

    ext2: avoid #ifdef __linux__ misuse in exportable headers
    Signed-off-by: Dave Martin <dave.martin@linaro.org>

--- include/linux/ext2_fs.h
+++ include/linux/ext2_fs.h
@@ -296,7 +296,7 @@ struct ext2_inode {
 
 #define i_size_high	i_dir_acl
 
-#if defined(__KERNEL__) || defined(__linux__)
+#if defined(__linux_kernel__) || defined(__linux__)
 #define i_reserved1	osd1.linux1.l_i_reserved1
 #define i_frag		osd2.linux2.l_i_frag
 #define i_fsize		osd2.linux2.l_i_fsize

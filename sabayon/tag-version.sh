bc7e7c5ec59a99183dba3572bb892bd72a554ae3
Fabio Erculiani <lxnay@sabayon.org>
2013-12-14 17:48:30 +0100

    sabayon: move remote server

--- sabayon/tag-version.sh
+++ sabayon/tag-version.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-www_host="$(whoami)@sabayon.org"
-www_port=2222
+# srv_host="$(whoami)@sabayon.org"
+# srv_port=2222
+# kernel_tag_script="/home/bin/kernel-tag-version"
+
+srv_host="$(whoami)@sabayon.ing.unibs.it"
+srv_port=8888
+kernel_tag_script="/scripts/kernel/kernel-tag-version"
 
 # Check target tag
 [[ -z "$1" ]] && echo "usage: $0 <new-version>" >&2 && exit 1
@@ -16,4 +21,4 @@ done
 echo "Tagging version: $tag"
 git tag "$tag" && git push origin HEAD && git push --tags || exit 1
 
-ssh -t -p "${www_port}" "${www_host}" screen /home/bin/kernel-tag-version "${tag}"
+ssh -t -p "${srv_port}" "${srv_host}" screen "${kernel_tag_script}" "${tag}"
